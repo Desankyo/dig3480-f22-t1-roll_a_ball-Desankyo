2b4eaf98fdd66b4cf73146e865563a3dac72ca14
Desankyo <112204628+Desankyo@users.noreply.github.com>
2022-09-16 01:49:03 +0000

    Sep 15, 2022, 9:49 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
     private int count;
     private float movementX;
     private float movementY;
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
